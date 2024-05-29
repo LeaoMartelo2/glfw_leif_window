24017475f5fc93ad254e3343120a144ba96632c0
LeaoMartelo <61511832+LeaoMartelo2@users.noreply.github.com>
2024-05-29 18:36:35 -0300

    main.c and build script

---
+++ build.sh
@@ -0,0 +1,2 @@
+set -xe
+cc *.c -lglfw -lGL -lleif -lm -lclipboard -o MAIN
---
+++ main.c
@@ -0,0 +1,38 @@
+#include <GL/gl.h>
+#include <GLFW/glfw3.h>
+#include <leif/leif.h>
+#include <stdio.h>
+
+#define WINW 400
+#define WINH 600
+
+int main() {
+
+  glfwInit();
+
+  GLFWwindow *window = glfwCreateWindow(WINW, WINH, "example", NULL, NULL);
+
+  glfwMakeContextCurrent(window);
+
+  lf_init_glfw(WINW, WINH, window);
+
+  while (!glfwWindowShouldClose(window)) {
+
+    glClear(GL_COLOR_BUFFER_BIT);
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+    lf_begin();
+
+    lf_text("Hello, World!\n");
+
+    lf_end();
+
+    glfwPollEvents();
+    glfwSwapBuffers(window);
+  }
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
+
+  return 0;
+}
